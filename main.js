31716673ae0208abef1a288783843414af23ec55
Daniel <dapascale@gmail.com>
2020-09-22 08:49:18 -0400

    added scroll to newly appended text

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ nasaApp.getDate = function (query) {
             <img src="${data.url}" alt="${data.title}" class="fit-content"><h2>Happy Birthday!</h2><p>There are trillions of stars in our universe, but today the brightest one is you!</p>
         `;
         // empty the image container and append a new image based on User input
-        $(".picture").empty().append(html).append(`<p class="learn-more-text">created at<a href="https://junocollege.com/"> Juno College<a/></p>`);
+        $(".picture").empty().append(html).append(`<p class="juno">created at<a href="https://junocollege.com/"> Juno College<a/></p>`);
         // run nasaApp.learnMore function
         nasaApp.learnMore(data);
     });
@@ -62,9 +62,12 @@ nasaApp.learnMore = function (data) {
     $('#info').on('click', function () {
         // create a variable to append to the page
         const explanation = `<p class="learn-more-text">${data.explanation}</p>`
-        $('#description').append(explanation).append(`<p class="learn-more-text">created at<a href="https://junocollege.com/"> Juno College<a/></p>`);
+        $('#description').append(explanation).append(`<p class="juno">created at<a href="https://junocollege.com/"> Juno College<a/></p>`);
         // After the button has been clicked, disable it
         $('#info').attr('disabled', true)
+        // create a scroll that takes users down to newly appended text
+        // this 'animate' portion of the code was originally written by user Edgar Villegas Alvarado in response to a Stack Overflow question.
+        $('body, html').animate({scrollTop: $('.learn-more-text').offset().top }, 1000);
     })
 }
 
